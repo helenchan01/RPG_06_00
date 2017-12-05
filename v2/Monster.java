ac8b77802ad5007c9f531a41be512b516cfe1564
Helen Chan <hchan5@stuy.edu>
2017-12-05 16:31:40 -0500

    Added toString

--- v2/Monster.java
+++ v2/Monster.java
@@ -6,6 +6,14 @@ public class Monster extends Character {
 	defense = 5;
 	attack = 2.1;
     }
+
+    public String toString() {
+	String stats = " hp: " + hp
+	             + " strength: " + strength
+	             + " defense: " + defense
+	             + " attack: " + attack;
+	return stats;
+    }
 }
 	    
 	
